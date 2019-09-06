e8b30cf4c5b7741a449f75586561b00de538bbf6
wslmacieira <wslmacieira@outlook.com>
2019-09-06 16:46:05 -0300

    adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,15 @@
+import { InMemoryDbService } from "angular-in-memory-web-api"
+
+export class InMemoryDatabase implements InMemoryDbService {
+    createDb(){
+        const categories = [
+            { id: 1, name: "Lazer", description: "Pagamentos de Contas da Casa"},
+            { id: 2, name: "Saúde", description: "Plano de saúde e Remédios"},
+            { id: 3, name: "Lazer", description: "Cinema, parques, praia, etc"},
+            { id: 4, name: "Salário", description: "Recebimento do Salário"},
+            { id: 5, name: "Freelas", description: "Trabalhos com freelancer"},
+        ];
+
+        return { categories }
+    }
+}
