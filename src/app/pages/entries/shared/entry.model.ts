267bff61ebf898a1bc2d628cf1ddade34e74201c
wslmacieira <wslmacieira@outlook.com>
2019-09-07 15:43:28 -0300

    adding EntryFormComponent

--- src/app/pages/entries/shared/entry.model.ts
+++ src/app/pages/entries/shared/entry.model.ts
@@ -3,7 +3,7 @@ import { Category } from "../../categories/shared/category.model";
 export class Entry {
     constructor(
         public id?: number,
-        public nome?: string,
+        public name?: string,
         public description?: string,
         public type?: string,
         public amount?: string,
