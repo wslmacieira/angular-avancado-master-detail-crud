cdbeb64875cf779b790dec3dca080a7d7c1a0208
wslmacieira <wslmacieira@outlook.com>
2019-09-06 23:57:41 -0300

    improving CategoryFormComponent

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -40,6 +40,14 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     this.setPageTitle()
   }
 
+  submitForm() {
+    this.submittingForm = true
+    if(this.currentAction == 'new')
+      this.createCategory()
+    else
+    this.updateCategory()
+  }
+
   // PRIVATE METHODS
   private setCurrentAction() {
     if (this.route.snapshot.url[0].path == "new")
@@ -79,6 +87,46 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
       const categoryName = this.category.name || ""
     this.pageTitle = "Editando Categoria: " + categoryName
     }
+  }
+
+  private createCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value)
+
+    this.categoryService.create(category)
+    .subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+  }
+
+  private updateCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value)
+
+    this.categoryService.update(category)
+    .subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+  }
+
+  private actionsForSuccess(category: Category) {
+    toastr.success("Solicitação processada com sucesso")
+
+    // redirect/reload component page
+    this.router.navigateByUrl("categories", {skipLocationChange: true}).then(
+      () => this.router.navigate(["categories",  category.id, "edit"])
+    )
+  }
+
+  private actionsForError(error) {
+    toastr.error("Ocorreu um erro ao processar sua colicitação!")
+
+    this.submittingForm = false
+
+    if(error.status === 422)
+    this.serverErrorMessages = JSON.parse(error._body).errors
+    else
+      this.serverErrorMessages = ["Falha na comunicação com servidor. Por favor, tente mais tarde"]
   }
 
 }
